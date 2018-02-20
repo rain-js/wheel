3cc23f522010cdf26e5264aeca2fbafce6128499
rain-js <unique_ritchie@163.com>
2018-02-21 00:28:49 +0800

    Sticky Plugin

---
+++ sticty/sticky.js
@@ -0,0 +1,36 @@
+class Sticky {
+	constructor(selector, n) {
+		this.$element = $(selector)
+		this.topOffset = n || 0
+		this.eleOffset = this.$element.offset()
+		this.addPlaceHolder()
+		this.listenToScroll()
+	}
+
+	addPlaceHolder() {
+		this.$element.wrap('<div class="sticky-wrapper"></div>')
+		this.$element.parent().height(this.$element.outerHeight())
+	}
+
+	listenToScroll() {
+		var that = this
+
+		$(window).on('scroll', function() {
+			console.log(that.topOffset)
+			if (window.scrollY + that.topOffset > that.eleOffset.top) {
+				that.$element.addClass('sticky')
+				that.$element.css({
+					top: that.topOffset
+				})
+			} else {
+				that.$element.removeClass('sticky')
+			}
+		})
+	}
+}
+
+$(document).ready(function() {
+	new Sticky('#topbar')
+	new Sticky('.button1', 80)
+	new Sticky('.button2', 150)
+})
